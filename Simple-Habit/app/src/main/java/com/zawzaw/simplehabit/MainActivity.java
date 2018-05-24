65d9ec7207ce03d9cba3a4e5f9874fcce975c024
ZawZaw <mczawzaw12@outlook.com>
2018-05-24 20:33:29 +0630

    Simple-Habit: add 'login' textview and full screen image with action bar

--- Simple-Habit/app/src/main/java/com/zawzaw/simplehabit/MainActivity.java
+++ Simple-Habit/app/src/main/java/com/zawzaw/simplehabit/MainActivity.java
@@ -2,6 +2,9 @@ package com.zawzaw.simplehabit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Window window = getWindow();
+        WindowManager.LayoutParams winParams = window.getAttributes();
+        winParams.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        window.setAttributes(winParams);
+        window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
 
     }
 
