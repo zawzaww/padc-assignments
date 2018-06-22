83564439f39ede08ec71792b8e3c04e4c1a4efab
Zaw Zaw <mczawzaw12@outlook.com>
2018-06-22 14:29:21 +0630

    TED-Talks Assignement: load images with Glide for speakers
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

---
+++ TED-Talks/app/src/main/java/com/zawzaw/tedtalksassignment/utils/MyAppGlideModule.java
@@ -0,0 +1,13 @@
+package com.zawzaw.tedtalksassignment.utils;
+
+import com.bumptech.glide.annotation.GlideModule;
+import com.bumptech.glide.module.AppGlideModule;
+
+/**
+ * Created by zawzaw on 22/06/2018.
+ */
+
+@GlideModule
+public class MyAppGlideModule extends AppGlideModule {
+
+}
--- TED-Talks/app/src/main/java/com/zawzaw/tedtalksassignment/viewholders/TalksViewHolder.java
+++ TED-Talks/app/src/main/java/com/zawzaw/tedtalksassignment/viewholders/TalksViewHolder.java
@@ -2,14 +2,16 @@ package com.zawzaw.tedtalksassignment.viewholders;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 import com.zawzaw.tedtalksassignment.R;
 import com.zawzaw.tedtalksassignment.data.vos.TalksVO;
 import com.zawzaw.tedtalksassignment.delegates.TalksDelegate;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import com.zawzaw.tedtalksassignment.utils.GlideApp;
 
 public class TalksViewHolder extends RecyclerView.ViewHolder {
 
@@ -17,8 +19,12 @@ public class TalksViewHolder extends RecyclerView.ViewHolder {
 
     private TalksVO mTalks;
 
-    @BindView(R.id.tv_speaker_name) TextView tvSpeakerName;
-    @BindView(R.id.tv_talk_title) TextView tvTalkTitle;
+    @BindView(R.id.tv_speaker_name)
+    TextView tvSpeakerName;
+    @BindView(R.id.tv_talk_title)
+    TextView tvTalkTitle;
+    @BindView(R.id.iv_speaker_img)
+    ImageView ivSpakerImage;
 
     public TalksViewHolder(View itemView, TalksDelegate talksDelegate) {
         super(itemView);
@@ -38,9 +44,14 @@ public class TalksViewHolder extends RecyclerView.ViewHolder {
     public void setTalksData(TalksVO talks) {
         mTalks = talks;
 
+        // Speaker name
         tvSpeakerName.setText(talks.getSpeaker().getName());
+        // Talk title
         tvTalkTitle.setText(talks.getTitle());
-
+        // Speaker's header image
+        GlideApp.with(ivSpakerImage.getContext())
+                .load(talks.getImageUrl())
+                .into(ivSpakerImage);
     }
 
 }
