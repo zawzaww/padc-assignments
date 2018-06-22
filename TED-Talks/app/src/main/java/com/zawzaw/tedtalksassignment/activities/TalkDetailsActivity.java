de68a45a9ada903344a7233d19695265a113227e
Zaw Zaw <mczawzaw12@outlook.com>
2018-06-23 01:14:30 +0630

    TED-Talks assignment: Live data binding for Talk details
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

--- TED-Talks/app/src/main/java/com/zawzaw/tedtalksassignment/activities/TalkDetailsActivity.java
+++ TED-Talks/app/src/main/java/com/zawzaw/tedtalksassignment/activities/TalkDetailsActivity.java
@@ -21,21 +21,22 @@ public class TalkDetailsActivity extends BaseActivity {
 
     @BindView(R.id.tv_talk_description) TextView tvTalkDecription;
     @BindView(R.id.iv_talk_image) ImageView ivTalkImage;
+    @BindView(R.id.tv_speaker) TextView tvSpeaker;
+    @BindView(R.id.tv_talk_title) TextView tvTalkTitle;
+    @BindView(R.id.tv_talk_duration) TextView tvDuration;
+    @BindView(R.id.iv_profile_pic) ImageView ivSpeakerProfileImage;
+    @BindView(R.id.tv_author_name) TextView tvAuthorName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_talk_details);
-
-        ButterKnife.bind(this, this);
-
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayShowHomeEnabled(false);
+
+        ButterKnife.bind(this, this);
 
         int talkId = getIntent().getIntExtra("talksId", 0);
         Log.d("TalkDetailsActiviy", "talksId : " + talkId);
@@ -52,14 +53,35 @@ public class TalkDetailsActivity extends BaseActivity {
     }
 
     private void bindData(TalksVO talks) {
-        // Talk description
+        // Talk description.
         tvTalkDecription.setText(talks.getDescription());
 
-        // Talk header image
+        // Talk header image.
         GlideApp.with(ivTalkImage.getContext())
                 .load(talks.getImageUrl())
                 .into(ivTalkImage);
 
+        // Speaker name.
+        tvSpeaker.setText(talks.getSpeaker().getName());
+
+        // Talk title.
+        tvTalkTitle.setText(talks.getTitle());
+
+        // Talk duration.
+        tvDuration.setText(secToString(talks.getDurationInSec()));
+
+        // Speaker Profile Image.
+        GlideApp.with(ivSpeakerProfileImage.getContext())
+                .load(talks.getImageUrl())
+                .into(ivSpeakerProfileImage);
+
+        // About the speaker
+        tvAuthorName.setText(talks.getSpeaker().getName());
+
+    }
+
+    public String secToString(int time) {
+        return String.format("%02d:%02d", time / 60, time % 60);
     }
 
 }
