fda3201d68d74a886a3015f2a36fcda5992b85d8
Zaw Zaw <mczawzaw12@outlook.com>
2018-06-27 15:30:30 +0630

    Simple-Habit: build.gradle: Update minSdkVersion and targetSdkVersion
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

--- Simple-Habit/app/src/main/java/com/zawzaw/simplehabit/MainActivity.java
+++ Simple-Habit/app/src/main/java/com/zawzaw/simplehabit/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Window window = getWindow();
         WindowManager.LayoutParams winParams = window.getAttributes();
         winParams.flags &= ~WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
