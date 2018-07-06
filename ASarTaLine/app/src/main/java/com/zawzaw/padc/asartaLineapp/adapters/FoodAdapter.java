a6ebc0da12f13c9966690a4cce7d573625c08497
Zaw Zaw <mczawzaw12@outlook.com>
2018-07-06 13:10:59 +0630

    ASarTaLine assignment: Add a new ViewHolder for offers food

--- ASarTaLine/app/src/main/java/com/zawzaw/padc/asartaLineapp/adapters/FoodAdapter.java
+++ ASarTaLine/app/src/main/java/com/zawzaw/padc/asartaLineapp/adapters/FoodAdapter.java
@@ -10,6 +10,7 @@ import com.zawzaw.padc.asartaLineapp.R;
 import com.zawzaw.padc.asartaLineapp.viewholders.BannerViewHolder;
 import com.zawzaw.padc.asartaLineapp.viewholders.BaseViewHolder;
 import com.zawzaw.padc.asartaLineapp.viewholders.FoodCatViewHolder;
+import com.zawzaw.padc.asartaLineapp.viewholders.OffersFoodViewHolder;
 
 /**
  * Created by zawzaw on 05/07/2018.
@@ -19,7 +20,7 @@ public class FoodAdapter extends RecyclerView.Adapter<BaseViewHolder> {
 
     private final int BANNER_VIEW = 1;
     private final int FOOD_CAT_VIEW = 2;
-    private final int RESTURANT_VIEW = 3;
+    private final int OFFERS_FOOD = 3;
 
     @NonNull
     @Override
@@ -35,8 +36,9 @@ public class FoodAdapter extends RecyclerView.Adapter<BaseViewHolder> {
                 View foodCatView = inflater.inflate(R.layout.view_holder_foodcat, parent, false);
                 return new FoodCatViewHolder(foodCatView);
 
-            case RESTURANT_VIEW:
-                return null;
+            case OFFERS_FOOD:
+                View offersFoodView = inflater.inflate(R.layout.view_holder_offersfood, parent, false);
+                return new OffersFoodViewHolder(offersFoodView);
         }
         return null;
     }
@@ -48,7 +50,7 @@ public class FoodAdapter extends RecyclerView.Adapter<BaseViewHolder> {
 
     @Override
     public int getItemCount() {
-        return 2;
+        return 9;
     }
 
     @Override
@@ -61,7 +63,7 @@ public class FoodAdapter extends RecyclerView.Adapter<BaseViewHolder> {
                 return FOOD_CAT_VIEW;
 
             default:
-                return RESTURANT_VIEW;
+                return OFFERS_FOOD;
         }
     }
 }
---
+++ ASarTaLine/app/src/main/java/com/zawzaw/padc/asartaLineapp/viewholders/OffersFoodViewHolder.java
@@ -0,0 +1,20 @@
+package com.zawzaw.padc.asartaLineapp.viewholders;
+
+import android.view.View;
+
+/**
+ * Created by zawzaw on 06/07/2018.
+ */
+
+public class OffersFoodViewHolder extends BaseViewHolder {
+
+    public OffersFoodViewHolder(View itemView) {
+        super(itemView);
+    }
+
+    @Override
+    public void bindData() {
+        super.bindData();
+    }
+
+}
