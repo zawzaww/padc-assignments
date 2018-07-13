092c4a18048c1b1ab79dcedca28b82f1499a096d
Zaw Zaw <mczawzaw12@outlook.com>
2018-07-13 08:32:06 +0630

    MM-Healthcare assignment: activities: Move to specific package and setup App bar
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

--- MM-Healthcare/app/src/main/java/com/zawzaw/padc/mmhealthcare/activities/HomeActivity.kt
+++ MM-Healthcare/app/src/main/java/com/zawzaw/padc/mmhealthcare/activities/HomeActivity.kt
@@ -1,14 +1,15 @@
-package com.zawzaw.padc.mmhealthcare
+package com.zawzaw.padc.mmhealthcare.activities
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import com.zawzaw.padc.mmhealthcare.R
 
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,18 +23,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         return when (item.itemId) {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
     }
+
 }
