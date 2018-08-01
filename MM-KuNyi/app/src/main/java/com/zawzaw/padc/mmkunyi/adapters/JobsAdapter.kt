165c4deff31d05bbddfc13d8e35c578c12e00ffd
Zaw Zaw <mczawzaw12@outlook.com>
2018-08-01 13:49:11 +0630

    MM-KuNyi: UI component: Resesigned UI design for ViewHolder items and implement adapter based View
    Signed-off-by: Zaw Zaw <mczawzaw12@outlook.com>

---
+++ MM-KuNyi/app/src/main/java/com/zawzaw/padc/mmkunyi/adapters/JobsAdapter.kt
@@ -0,0 +1,35 @@
+package com.zawzaw.padc.mmkunyi.adapters
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.zawzaw.padc.mmkunyi.R
+import com.zawzaw.padc.mmkunyi.viewholders.JobsViewHolder
+
+/**
+ * Created by zawzaw on 01/08/2018.
+ */
+
+class JobsAdapter() : RecyclerView.Adapter<JobsViewHolder>() {
+
+    init {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): JobsViewHolder {
+        val layoutInflater: LayoutInflater = LayoutInflater.from(parent.context)
+        val view: View = layoutInflater.inflate(R.layout.view_holder_jobs, parent, false)
+
+        return JobsViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: JobsViewHolder, position: Int) {
+
+    }
+
+    override fun getItemCount(): Int {
+        return 9
+    }
+
+}
---
+++ MM-KuNyi/app/src/main/java/com/zawzaw/padc/mmkunyi/viewholders/JobsViewHolder.kt
@@ -0,0 +1,12 @@
+package com.zawzaw.padc.mmkunyi.viewholders
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+/**
+ * Created by zawzaw on 01/08/2018.
+ */
+
+class JobsViewHolder(itemView: View?) : RecyclerView.ViewHolder(itemView) {
+
+}
